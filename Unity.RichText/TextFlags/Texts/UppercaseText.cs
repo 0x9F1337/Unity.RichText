9f43251d391a4255f75d1d5f9f4acb91dedf0e69
0x9F <30807136+0x9F1337@users.noreply.github.com>
2023-09-19 21:16:53 +0200

    Add UppercaseText

---
+++ Unity.RichText/TextFlags/Texts/UppercaseText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.RichText.TextFlags.Interfaces;
+
+namespace Unity.RichText.TextFlags.Texts
+{
+    [TextItemIdentifier( UnityRichTextFlag.Uppercase )]
+    internal class UppercaseText : ITextItem
+    {
+        public string? Param => throw new NotImplementedException();
+
+        public string CloseTag()
+            => "</uppercase>";
+
+        public string OpenTag()
+            => "<uppercase>";
+    }
+}
